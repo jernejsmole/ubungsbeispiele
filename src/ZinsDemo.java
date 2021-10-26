9ab45e15fc29ad71544251c77fdde2915e515a54
jernejsmole <jersmole@gmail.com>
2021-10-26 07:12:07 +0200

    fertig 1

---
+++ src/ZinsDemo.java
@@ -0,0 +1,8 @@
+public class ZinsDemo {
+    public static void main(String[] args) {
+        double kontostand = 1000;
+        double zinsen = 2.25;
+
+        System.out.println("Kosten der zinsen sind " + kontostand * zinsen / 100 + "€");
+    }
+}
